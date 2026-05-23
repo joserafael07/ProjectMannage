b2f4f440bdafeddf5f915186a69c43f0137134ab
joserafael2 <jose.rafael@estudante.ifmt.edu.br>
2026-05-23 18:43:47 -0400

    Criando Applicationservice

---
+++ projectManager-main/projectManager-main/src/main/java/com/projectmanager/ProjectMannage/applicationservice/ProjectService.java
@@ -0,0 +1,35 @@
+package com.projectmanager.ProjectMannage.applicationservice;
+
+import com.projectmanager.ProjectMannage.domain.entity.Project;
+import com.projectmanager.ProjectMannage.infrasctructure.dto.dto.SaveProjectDataDTO;
+import com.projectmanager.ProjectMannage.model.ProjectStatus;
+import com.projectmanager.ProjectMannage.repository.ProjectRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class ProjectService {
+
+    private final ProjectRepository projectRepository;
+
+    @Transactional
+    public Project createProject(SaveProjectDataDTO saveProjectDataDTO) {
+        Project project = Project
+                .builder()
+                .name(saveProjectDataDTO.getName())
+                .description(saveProjectDataDTO.getDescription())
+                .initialDate(saveProjectDataDTO.getInitialDate())
+                .finalDate(saveProjectDataDTO.getFinalDate())
+                .status(ProjectStatus.PEDDING)
+                .build();
+
+        return project;
+    }
+
+}
+
+
+
